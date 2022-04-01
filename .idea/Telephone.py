8325a7e17ca2096fd32fb03fc05fe267cdf1b022
madsrbl270900 <madsrbl@outlook.dk>
2022-04-02 00:01:23 +0200

    tilføjet forbindelse til raspary pi og vores lys

--- .idea/Telephone.py
+++ .idea/Telephone.py
@@ -4,21 +4,34 @@ from scipy.io.wavfile import write
 import os, os.path
 import keyboard  # using module keyboard
 import random
+#import RPi.GPIO as GPIO
 
 #setup of recording
 fs = 44100  # Sample rate
 seconds = 5  # Duration of recording
+path = "C:/Users/mads/IdeaProjects/telefonprojekt/.idea/Lyd"
+#brugt for at læse pin 4 af raspary pi og hvis den bliver trykket på giver den false eller true
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(18, GPIO.OUT)
+#GPIO.setup(31, GPIO.IN)
+#herunder læser vi vores input
+#print GPIO.input(31)
+#skriver high eller low til en pin som vores lys kontorler kan læse (arduino)
+#GPIO.output(18, GPIO.HIGH)
+
+#husk at ryde op når kode slutter
+#GPIO.cleanup()
 
 
 while(True):
         if keyboard.is_pressed('q'):  # if key 'q' is pressed
             #afspil tilfældig optaget lyd
-            list = os.listdir('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd')
+            list = os.listdir(path)
             number_lyd = len(list)
             tilfoldigt_nr = random.randint(1,number_lyd-1)
             print(tilfoldigt_nr)
 
-            playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/lyd" + str(tilfoldigt_nr) + ".wav")
+            playsound(path + "/lyd" + str(tilfoldigt_nr) + ".wav")
 
             #optager
             myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
@@ -26,12 +39,12 @@ while(True):
             sd.wait()  # Wait until recording is finished
 
             #gemmer
-            list = os.listdir('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd') # dir is your directory path
+            list = os.listdir(path) # dir is your directory path
             number_lyd = len(list)
             navn = "lyd" + str(number_lyd) + ".wav"
             write('Lyd/' + navn, fs, myrecording)  # Save as WAV file
             print("optager færdig")
 
             #afspiller skal fjernes anvends til test pt
-            playsound('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/lyd' + str(number_lyd) + '.wav')
+            playsound(path + '/lyd' + str(number_lyd) + '.wav')
             print("afspil færdig")
