d7ab422f3bb5fc13bfe1a725ece49cf4c17e3ad7
mrbl270900 <90680832+mrbl270900@users.noreply.github.com>
2022-04-08 14:38:57 +0200

    virker telefon

--- .idea/Telephone.py
+++ .idea/Telephone.py
@@ -17,38 +17,38 @@ while(True):
         sd.wait()  # Wait until recording is finished
 
         #gemmer
-        write('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/info2.wav', fs, myrecording)  # Save as WAV file
+        write('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/info1.wav', fs, myrecording)  # Save as WAV file
         print("optager færdig")
 
     if keyboard.is_pressed('q'):  # if key 'q' is pressed
-        #afspiller lyd vi har optaget til start inf
-        playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/info1.wav")
+        #afspiller lyd vi har optaget til start info
+        playsound("C://Users//madsr//IdeaProjects//telephonetest//.idea//Lyd//info1.wav")
 
 
         #afspil tilfældig optaget lyd
         list = os.listdir('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd')
         number_lyd = len(list)
-        tilfoldigt_nr = random.randint(1,number_lyd-1)
+        tilfoldigt_nr = random.randint(1,number_lyd-3)
         print(tilfoldigt_nr)
 
         playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/lyd" + str(tilfoldigt_nr) + ".wav")
 
         #afspiller lyd før optagelse
-        playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/info2.wav")
+        playsound("C://Users//madsr//IdeaProjects//telephonetest//.idea//Lyd//info2.wav")
 
         #optager
+        playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/Answering_machine_beep.wav")
         myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-        playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/ding-sound-effect_2.mp3")
         print("optager")
         sd.wait()  # Wait until recording is finished
+        playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/Answering_machine_beep.wav")
 
         #gemmer
-        list = os.listdir('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd') # dir is your directory path
-        number_lyd = len(list)
+        list = os.listdir('C://Users//madsr//IdeaProjects//telephonetest//.idea//Lyd') # dir is your directory path
+        number_lyd = len(list)-2
         navn = "lyd" + str(number_lyd) + ".wav"
         write('Lyd/' + navn, fs, myrecording)  # Save as WAV file
         print("optager færdig")
-        playsound("C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/ding-sound-effect_2.mp3")
 
         #afspiller skal fjernes, anvends til test pt
         #playsound('C:/Users/madsr/IdeaProjects/telephonetest/.idea/Lyd/lyd' + str(number_lyd) + '.wav')
